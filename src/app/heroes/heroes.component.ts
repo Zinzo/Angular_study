d1c694b4cd7705510b328356a94965d1342e9a8a
jaehongjun <gatsby0391@naver.com>
2018-04-07 15:18:15 +0900

    4장 ngFor 반목문 사용하여 여래개 데이터 보여주기 / let

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+// import {Hero} from '../hero';
+import {HEROES} from '../HEROES';
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +14,12 @@ export class HeroesComponent implements OnInit {
     name: 'Winstorm'
   };*/
 
-  hero = new Hero();
+  // hero = new Hero();
+  heroes = HEROES;
 
   constructor() {
-    this.hero.id = 1;
-    this.hero.name = 'WinStorm';
+   /* this.hero.id = 1;
+    this.hero.name = 'WinStorm';*/
   }
 
   ngOnInit() {
